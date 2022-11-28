7482b36bd7a43d8ed336ed8440dd4622a5941c60
Brice Dobry <brice@hiro.so>
2022-11-28 16:46:23 -0500

    test: test `stx-account` after stacking

--- tests/integration/clarity/natives/stx-account.spec.ts
+++ tests/integration/clarity/natives/stx-account.spec.ts
@@ -17,9 +17,15 @@ import {
   StacksDevnetOrchestrator,
   StacksTransactionMetadata,
 } from "@hirosystems/stacks-devnet-js";
-import { broadcastStackSTX } from "../../helpers";
+import {
+  broadcastStackSTX,
+  getBitcoinBlockHeight,
+  waitForNextPreparePhase,
+  waitForNextRewardPhase,
+  waitForStacksChainUpdate,
+} from "../../pox/helpers";
 
-const STACKS_2_1 = 107;
+const STACKS_2_1 = 114;
 
 const orchestrator: StacksDevnetOrchestrator = new StacksDevnetOrchestrator({
   path: "./Clarinet.toml",
@@ -27,8 +33,9 @@ const orchestrator: StacksDevnetOrchestrator = new StacksDevnetOrchestrator({
   devnet: {
     bitcoin_controller_block_time: Constants.BITCOIN_BLOCK_TIME,
     epoch_2_0: 100,
-    epoch_2_05: 100,
+    epoch_2_05: 107,
     epoch_2_1: STACKS_2_1,
+    pox_stacking_orders: [],
   },
 });
 
@@ -36,13 +43,13 @@ beforeAll(() => orchestrator.start());
 afterAll(() => orchestrator.stop());
 
 test("New method 'stx-account' errors in 2.05 and works in 2.1", async () => {
+  const network = new StacksTestnet({ url: orchestrator.getStacksNodeUrl() });
+
   // Wait for Stacks genesis and boot contracts blocks to be mined
   orchestrator.waitForStacksBlock();
-  let chainEvent: StacksChainUpdate = orchestrator.waitForStacksBlock();
-  let blockHeight = chainEvent.new_blocks[0].block.block_identifier.index;
+  let chainUpdate: StacksChainUpdate = orchestrator.waitForStacksBlock();
+  let blockHeight = chainUpdate.new_blocks[0].block.block_identifier.index;
   expect(blockHeight).toBe(2);
-
-  const network = new StacksTestnet({ url: orchestrator.getStacksNodeUrl() });
 
   // Build the transaction to deploy the contract
   let deployTxOptions = {
@@ -66,27 +73,27 @@ test("New method 'stx-account' errors in 2.05 and works in 2.1", async () => {
 
   // Wait for the next block, and check that the transaction is included
   // and that it failed.
-  chainEvent = orchestrator.waitForStacksBlock();
+  chainUpdate = orchestrator.waitForStacksBlock();
   expect(
-    (<StacksBlockMetadata>chainEvent.new_blocks[0].block.metadata)
+    (<StacksBlockMetadata>chainUpdate.new_blocks[0].block.metadata)
       .bitcoin_anchor_block_identifier.index
   ).toBeLessThan(STACKS_2_1);
   expect(
     (<StacksTransactionMetadata>(
-      chainEvent.new_blocks[0].block.transactions[1].metadata
+      chainUpdate.new_blocks[0].block.transactions[1].metadata
     )).description
   ).toBe(`deployed: ${Accounts.DEPLOYER.stxAddress}.test-2-05`);
   expect(
     (<StacksTransactionMetadata>(
-      chainEvent.new_blocks[0].block.transactions[1].metadata
+      chainUpdate.new_blocks[0].block.transactions[1].metadata
     )).success
   ).toBeFalsy();
 
   // Wait for 2.1 to go live
   while (true) {
-    let chainEvent: StacksChainUpdate = orchestrator.waitForStacksBlock();
+    let chainUpdate: StacksChainUpdate = orchestrator.waitForStacksBlock();
     let bitcoinHeight = (<StacksBlockMetadata>(
-      chainEvent.new_blocks[0].block.metadata
+      chainUpdate.new_blocks[0].block.metadata
     )).bitcoin_anchor_block_identifier.index;
     if (bitcoinHeight >= STACKS_2_1) break;
   }
@@ -113,15 +120,15 @@ test("New method 'stx-account' errors in 2.05 and works in 2.1", async () => {
 
   // Wait for the next block, and check that the transaction is included
   // and that it succeeded.
-  chainEvent = orchestrator.waitForStacksBlock();
+  chainUpdate = orchestrator.waitForStacksBlock();
   expect(
     (<StacksTransactionMetadata>(
-      chainEvent.new_blocks[0].block.transactions[1].metadata
+      chainUpdate.new_blocks[0].block.transactions[1].metadata
     )).description
   ).toBe(`deployed: ${Accounts.DEPLOYER.stxAddress}.test-2-1`);
   expect(
     (<StacksTransactionMetadata>(
-      chainEvent.new_blocks[0].block.transactions[1].metadata
+      chainUpdate.new_blocks[0].block.transactions[1].metadata
     )).success
   ).toBeTruthy();
 
@@ -146,39 +153,73 @@ test("New method 'stx-account' errors in 2.05 and works in 2.1", async () => {
 
   // Wait for the next block, and check that the transaction is included
   // and that it succeeded with the proper result.
-  chainEvent = orchestrator.waitForStacksBlock();
+  chainUpdate = orchestrator.waitForStacksBlock();
   expect(
     (<StacksTransactionMetadata>(
-      chainEvent.new_blocks[0].block.transactions[1].metadata
+      chainUpdate.new_blocks[0].block.transactions[1].metadata
     )).description
   ).toBe(`invoked: ${Accounts.DEPLOYER.stxAddress}.test-2-1::test()`);
   expect(
     (<StacksTransactionMetadata>(
-      chainEvent.new_blocks[0].block.transactions[1].metadata
+      chainUpdate.new_blocks[0].block.transactions[1].metadata
     )).result
   ).toBe(
     "(ok (tuple (locked u0) (unlock-height u0) (unlocked u99999999998000)))"
   );
   expect(
     (<StacksTransactionMetadata>(
-      chainEvent.new_blocks[0].block.transactions[1].metadata
+      chainUpdate.new_blocks[0].block.transactions[1].metadata
     )).success
   ).toBeTruthy();
+});
 
-  // Lock some stacks, then call again and check the results
-  // Build a `stack-stx` transaction
-  blockHeight = chainEvent.new_blocks[0].block.block_identifier.index;
-  result = await broadcastStackSTX(
+test("stx-account after stacking", async () => {
+  const network = new StacksTestnet({ url: orchestrator.getStacksNodeUrl() });
+
+  // Wait for 2.1
+  waitForStacksChainUpdate(orchestrator, STACKS_2_1);
+
+  // Build the transaction to deploy the contract
+  let deployTxOptions = {
+    senderKey: Accounts.DEPLOYER.secretKey,
+    contractName: "test-2-1",
+    codeBody: `(define-public (test)
+    (ok (stx-account tx-sender))
+)`,
+    fee: 2000,
+    nonce: 1,
+    network,
+    anchorMode: AnchorMode.OnChainOnly,
+    postConditionMode: PostConditionMode.Allow,
+  };
+  let transaction = await makeContractDeploy(deployTxOptions);
+
+  // Broadcast transaction
+  let result = await broadcastTransaction(transaction, network);
+  expect((<TxBroadcastResultOk>result).error).toBeUndefined();
+
+  // Wait for block N-2 where N is the height of the next prepare phase
+  let chainUpdate = await waitForNextPreparePhase(network, orchestrator, -2);
+  let blockHeight = getBitcoinBlockHeight(chainUpdate);
+
+  // Broadcast some STX stacking orders
+  console.log("Broadcasting stacking orders");
+  let response = await broadcastStackSTX(
     2,
     network,
-    50_000_000_000_000,
+    25_000_000_000_000,
     Accounts.WALLET_1,
     blockHeight
   );
-  console.log(result);
-  chainEvent = orchestrator.waitForStacksBlock();
+  expect(response.error).toBeUndefined();
 
-  callTxOptions = {
+  chainUpdate = orchestrator.waitForStacksBlock();
+
+  // Wait for block N+1 where N is the height of the next reward phase
+  chainUpdate = await waitForNextRewardPhase(network, orchestrator, 1);
+
+  // Build a transaction to call the contract
+  let callTxOptions: SignedContractCallOptions = {
     senderKey: Accounts.WALLET_1.secretKey,
     contractAddress: Accounts.DEPLOYER.stxAddress,
     contractName: "test-2-1",
@@ -190,31 +231,31 @@ test("New method 'stx-account' errors in 2.05 and works in 2.1", async () => {
     anchorMode: AnchorMode.OnChainOnly,
     postConditionMode: PostConditionMode.Allow,
   };
-  console.log("make contract call...");
   transaction = await makeContractCall(callTxOptions);
-  console.log("broadcast transaction...");
+
+  // Broadcast transaction
   result = await broadcastTransaction(transaction, network);
   expect((<TxBroadcastResultOk>result).error).toBeUndefined();
 
   // Wait for the next block, and check that the transaction is included
   // and that it succeeded with the proper result.
   console.log("wait for stacks block...");
-  chainEvent = orchestrator.waitForStacksBlock();
+  chainUpdate = orchestrator.waitForStacksBlock();
   expect(
     (<StacksTransactionMetadata>(
-      chainEvent.new_blocks[0].block.transactions[1].metadata
+      chainUpdate.new_blocks[0].block.transactions[1].metadata
     )).description
   ).toBe(`invoked: ${Accounts.DEPLOYER.stxAddress}.test-2-1::test()`);
   expect(
     (<StacksTransactionMetadata>(
-      chainEvent.new_blocks[0].block.transactions[1].metadata
+      chainUpdate.new_blocks[0].block.transactions[1].metadata
     )).result
   ).toBe(
-    "(ok (tuple (locked u50000000000000) (unlock-height u0) (unlocked u50000000000000)))"
+    "(ok (tuple (locked u25000000000000) (unlock-height u0) (unlocked u75000000000000)))"
   );
   expect(
     (<StacksTransactionMetadata>(
-      chainEvent.new_blocks[0].block.transactions[1].metadata
+      chainUpdate.new_blocks[0].block.transactions[1].metadata
     )).success
   ).toBeTruthy();
 });
