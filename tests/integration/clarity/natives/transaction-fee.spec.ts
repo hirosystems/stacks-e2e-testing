49fa090ca420ac1a81cbf6df26d0bd7965a4a68c
Mårten Blankfors <marten@blankfors.se>
2022-12-20 13:33:36 +0100

    refactor: Broke out contract deployment to helper function

--- tests/integration/clarity/natives/transaction-fee.spec.ts
+++ tests/integration/clarity/natives/transaction-fee.spec.ts
@@ -1,67 +1,70 @@
 import {
-  makeContractDeploy,
-  broadcastTransaction,
-  AnchorMode,
-  PostConditionMode,
-  TxBroadcastResultOk,
-  makeContractCall,
-  SignedContractCallOptions,
-  ClarityVersion,
+    makeContractDeploy,
+    broadcastTransaction,
+    AnchorMode,
+    PostConditionMode,
+    TxBroadcastResultOk,
+    makeContractCall,
+    SignedContractCallOptions,
+    ClarityVersion,
 } from "@stacks/transactions";
 import { StacksNetwork, StacksTestnet } from "@stacks/network";
 import { Accounts, Constants } from "../../constants";
 import { principalCV } from "@stacks/transactions/dist/clarity/types/principalCV";
 import {
-  buildDevnetNetworkOrchestrator,
-  waitForStacksChainUpdate,
-  waitForStacksTransaction,
+    buildDevnetNetworkOrchestrator,
+    waitForStacksChainUpdate,
+    waitForStacksTransaction,
 } from "../../helpers";
 import { DevnetNetworkOrchestrator } from "@hirosystems/stacks-devnet-js";
 
 describe("transaction-fee", () => {
 
     test("fee is charged prior to execution", async () => {
-      const orchestrator = buildDevnetNetworkOrchestrator();
-      orchestrator.start();
-      const network = new StacksTestnet({ url: orchestrator.getStacksNodeUrl() });
+        const orchestrator = buildDevnetNetworkOrchestrator();
+        orchestrator.start();
+        const network = new StacksTestnet({ url: orchestrator.getStacksNodeUrl() });
 
-      // Wait for Stacks 2.1 to start
-      waitForStacksChainUpdate(orchestrator, Constants.DEVNET_DEFAULT_EPOCH_2_1);
+        // Wait for Stacks 2.1 to start
+        waitForStacksChainUpdate(orchestrator, Constants.DEVNET_DEFAULT_EPOCH_2_1);
 
-      let deployTxOptions = {
-        senderKey: Accounts.DEPLOYER.secretKey,
-        contractName: "test-2-1",
-        codeBody: `(define-public (test (p bool))
-    (ok p)
-  )`,
-        fee: 2000,
-        network,
-        anchorMode: AnchorMode.OnChainOnly,
-        clarityVersion: ClarityVersion.Clarity1,
-        postConditionMode: PostConditionMode.Allow,
-      };
+        let deploy = deployContract(orchestrator, network);
+        let tx = await deploy("test-2-1", "(define-public (test (p bool)) (ok p))");
 
-      let transaction = await makeContractDeploy(deployTxOptions);
+        expect(tx.description).toBe(
+            `deployed: ${Accounts.DEPLOYER.stxAddress}.test-2-1`
+        );
+    
+        expect(tx.success).toBeTruthy();
 
-      // Broadcast transaction
-      let result = await broadcastTransaction(transaction, network);
-      expect((<TxBroadcastResultOk>result).error).toBeUndefined();
-
-      // Wait for the transaction to be processed
-      let [block, tx] = waitForStacksTransaction(
-        orchestrator,
-        Accounts.DEPLOYER.stxAddress
-      );
-      expect(tx.description).toBe(
-        `deployed: ${Accounts.DEPLOYER.stxAddress}.test-2-1`
-      );
-      console.log(tx);
-      expect(tx.success).toBeTruthy();
-
-      orchestrator.stop();
+        orchestrator.stop();
     });
 });
 
-//function deployContract() {
-//  
-//}
+function deployContract(orchestrator : DevnetNetworkOrchestrator, network : StacksNetwork) {
+    return async (contractName : string, codeBody : string) => {
+        let deployTxOptions = {
+            senderKey: Accounts.DEPLOYER.secretKey,
+            contractName,
+            codeBody,
+            fee: 2000,
+            network,
+            anchorMode: AnchorMode.OnChainOnly,
+            clarityVersion: ClarityVersion.Clarity1,
+            postConditionMode: PostConditionMode.Allow,
+        };
+
+        let transaction = await makeContractDeploy(deployTxOptions);
+
+        // Broadcast transaction
+        let result = await broadcastTransaction(transaction, network);
+        expect((<TxBroadcastResultOk>result).error).toBeUndefined();
+
+        // Wait for the transaction to be processed
+        let [_block, tx] = waitForStacksTransaction(
+            orchestrator,
+            Accounts.DEPLOYER.stxAddress
+        );
+        return tx;
+    }
+}
