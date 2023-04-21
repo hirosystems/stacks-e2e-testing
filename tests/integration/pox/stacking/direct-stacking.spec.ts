20ca0f65b197ba9251a2f663d22337dfc3e59445
friedger <mail@friedger.de>
2023-04-21 20:01:06 +0200

    chore: add more asserts

--- tests/integration/pox/stacking/direct-stacking.spec.ts
+++ tests/integration/pox/stacking/direct-stacking.spec.ts
@@ -1,19 +1,18 @@
+import { DevnetNetworkOrchestrator } from "@hirosystems/stacks-devnet-js";
+import { StacksTestnet } from "@stacks/network";
+import { Accounts } from "../../constants";
 import {
   buildDevnetNetworkOrchestrator,
-  getBitcoinBlockHeight,
-  getNetworkIdFromEnv,
+  getNetworkIdFromEnv
 } from "../../helpers";
 import {
-  waitForNextPreparePhase,
-  waitForNextRewardPhase,
   getPoxInfo,
-  waitForRewardCycleId,
+  waitForNextRewardPhase
 } from "../helpers";
-import { Accounts } from "../../constants";
-import { StacksTestnet } from "@stacks/network";
-import { DevnetNetworkOrchestrator } from "@hirosystems/stacks-devnet-js";
-import { broadcastStackIncrease, broadcastStackSTX, broadcastStackExtend } from "../helpers-direct-stacking";
-import { expectAccountToBe } from "../helpers.ts";
+import {
+  broadcastStackIncrease,
+  broadcastStackSTX
+} from "../helpers-direct-stacking";
 
 describe("testing stacking under epoch 2.1", () => {
   let orchestrator: DevnetNetworkOrchestrator;
@@ -48,13 +47,13 @@ describe("testing stacking under epoch 2.1", () => {
     let response = await broadcastStackSTX(
       2,
       network,
-      150_000_000_000_000,
+      50_000_000_000_000,
       Accounts.WALLET_1,
       blockHeight,
       cycles,
       fee,
       0
-    )
+    );
     expect(response.error).toBeUndefined();
 
     response = await broadcastStackSTX(
@@ -66,7 +65,7 @@ describe("testing stacking under epoch 2.1", () => {
       cycles,
       fee,
       0
-    )
+    );
     expect(response.error).toBeUndefined();
 
     response = await broadcastStackIncrease(
@@ -75,26 +74,35 @@ describe("testing stacking under epoch 2.1", () => {
       Accounts.WALLET_2,
       fee,
       1
-    )
+    );
     expect(response.error).toBeUndefined();
 
-    await orchestrator.waitForNextStacksBlock()
+    await orchestrator.waitForNextStacksBlock();
     const poxInfo = await getPoxInfo(network);
 
-    // Assert
+    // Asserts about pox info for better knowledge sharing
     expect(poxInfo.contract_id).toBe("ST000000000000000000002AMW42H.pox-2");
-    expect(poxInfo.pox_activation_threshold_ustx).toBe(50_000_000_000_000);
+    expect(poxInfo.pox_activation_threshold_ustx).toBe(50_286_942_145_278);
     expect(poxInfo.current_cycle.id).toBe(1);
     expect(poxInfo.current_cycle.min_threshold_ustx).toBe(20_960_000_000_000);
-    expect(poxInfo.current_cycle.stacked_ustx).toBe(150_000_000_000_000);
-    expect(poxInfo.current_cycle.is_pox_active).toBe(true);
 
+    // Assert that the next cycle has 100m STX locked
+    expect(poxInfo.current_cycle.stacked_ustx).toBe(0);
+    expect(poxInfo.next_cycle.stacked_ustx).toBe(100_000_000_000_000);
+    expect(poxInfo.current_cycle.is_pox_active).toBe(false);
+
+    // move on to the nexte cycle
     await waitForNextRewardPhase(network, orchestrator, 1);
 
-        // Assert
-        expect(poxInfo.contract_id).toBe("ST000000000000000000002AMW42H.pox-2");
-        expect(poxInfo.current_cycle.total_stacked).toBe(150_000_000_000_000);
-        expect(poxInfo.current_cycle.is_pox_active).toBe(true);
-    
+    // Assert that the current cycle has 100m STX locked and earning
+    expect(poxInfo.current_cycle.id).toBe(1);
+    expect(poxInfo.current_cycle.stacked_ustx).toBe(100_000_000_000_000);
+    expect(poxInfo.current_cycle.is_pox_active).toBe(true);
+
+    // move on to the nexte cycle
+    await waitForNextRewardPhase(network, orchestrator, 1);
+
+    // Assert reward slots
+    // TODO
   });
 });
