3e6de66cce5187ab6262901bb6b0623e200c5f1b
friedger <mail@friedger.de>
2023-05-01 20:14:48 +0200

    feat: add test to call public function as readonly

--- tests/integration/epoch-2-2/bad-function-argument.spec.ts
+++ tests/integration/epoch-2-2/bad-function-argument.spec.ts
@@ -11,8 +11,10 @@ import {
   broadcastTransaction,
   callReadOnlyFunction,
   contractPrincipalCV,
+  cvToString,
   makeContractCall,
   makeContractDeploy,
+  responseOkCV,
   uintCV,
 } from "@stacks/transactions";
 import { Accounts } from "../constants";
@@ -21,7 +23,10 @@ import {
   asyncExpectStacksTransactionSuccess,
   buildDevnetNetworkOrchestrator,
   getNetworkIdFromEnv,
+  waitForStacksTransaction,
 } from "../helpers";
+
+const fee = 2000;
 
 describe("trait parameter", () => {
   let orchestrator: DevnetNetworkOrchestrator;
@@ -50,107 +55,144 @@ describe("trait parameter", () => {
     orchestrator.terminate();
   });
 
-  it("passing a trait parameter should work", async () => {
+  it("passing a trait parameter should work in Stacks 2.1", async () => {
     const network = new StacksTestnet({ url: orchestrator.getStacksNodeUrl() });
+    const contractAddress = Accounts.DEPLOYER.stxAddress;
 
     await orchestrator.waitForNextStacksBlock();
 
-    let codeBody = `(define-trait test-trait
-      ((foo (uint) (response uint uint))))
-    
-    (define-read-only (foo-arg (f <test-trait>))
-      (contract-of f)
-    )
-    
-    (define-public (call-foo (f <test-trait>) (a uint))
-      (contract-call? f foo a)
-    )`;
-
-    // Build the transaction to deploy the contract
-    let deployTxOptions = {
-      senderKey: Accounts.DEPLOYER.secretKey,
-      contractName: "test-trait",
-      codeBody,
-      fee: 2000,
+    let deployResponse = await deployContract(
       network,
-      anchorMode: AnchorMode.OnChainOnly,
-      postConditionMode: PostConditionMode.Allow,
-      nonce: 0,
-      clarityVersion: undefined,
-    };
+      Accounts.DEPLOYER.secretKey,
+      0,
+      "test-trait",
+      `(define-trait test-trait
+        ((foo (uint) (response uint uint))))
+      
+      (define-read-only (foo-arg (f <test-trait>))
+        (contract-of f)
+      )
+      
+      (define-public (call-foo (f <test-trait>) (a uint))
+        (contract-call? f foo a)
+      )`
+    );
+    await asyncExpectStacksTransactionSuccess(
+      orchestrator,
+      deployResponse.transaction.txid()
+    );
 
-    let transaction = await makeContractDeploy(deployTxOptions);
-    let response = await broadcastTransaction(transaction, network);
-    expect(response.error).toBeUndefined();
-    await asyncExpectStacksTransactionSuccess(orchestrator, transaction.txid());
-
-    codeBody = `(define-public (foo (a uint))
+    deployResponse = await deployContract(
+      network,
+      Accounts.DEPLOYER.secretKey,
+      1,
+      "impl-trait",
+      `(define-public (foo (a uint))
     (ok a)
-  )`;
+  )`
+    );
 
-    // Build the transaction to deploy the contract
-    deployTxOptions = {
-      senderKey: Accounts.DEPLOYER.secretKey,
-      contractName: "impl-trait",
-      codeBody,
-      fee: 2000,
-      network,
-      anchorMode: AnchorMode.OnChainOnly,
-      postConditionMode: PostConditionMode.Allow,
-      nonce: 1,
-      clarityVersion: undefined,
-    };
-
-    transaction = await makeContractDeploy(deployTxOptions);
-    response = await broadcastTransaction(transaction, network);
-    expect(response.error).toBeUndefined();
-    await asyncExpectStacksTransactionSuccess(orchestrator, transaction.txid());
+    await asyncExpectStacksTransactionSuccess(
+      orchestrator,
+      deployResponse.transaction.txid()
+    );
 
     await orchestrator.waitForNextStacksBlock();
 
     // Call the readonly function
     let output = await callReadOnlyFunction({
       contractName: "test-trait",
-      contractAddress: Accounts.DEPLOYER.stxAddress,
+      contractAddress,
       functionName: "foo-arg",
+      functionArgs: [contractPrincipalCV(contractAddress, "impl-trait")],
+      network,
+      senderAddress: Accounts.WALLET_1.stxAddress,
+    });
+    expect(output, cvToString(output)).toEqual(
+      contractPrincipalCV(contractAddress, "impl-trait")
+    );
+
+    // call public function as readonly
+    output = await callReadOnlyFunction({
+      contractName: "test-trait",
+      contractAddress,
+      functionName: "call-foo",
       functionArgs: [
-        contractPrincipalCV(Accounts.DEPLOYER.stxAddress, "impl-trait"),
+        contractPrincipalCV(contractAddress, "impl-trait"),
+        uintCV(1),
       ],
       network,
       senderAddress: Accounts.WALLET_1.stxAddress,
     });
-    expect(output).toEqual(
-      contractPrincipalCV(Accounts.DEPLOYER.stxAddress, "impl-trait")
-    );
+    expect(output, cvToString(output)).toEqual(responseOkCV(uintCV(1)));
 
     // Call the public function
     let callTxOptions: SignedContractCallOptions = {
       senderKey: Accounts.WALLET_1.secretKey,
-      contractAddress: Accounts.DEPLOYER.stxAddress,
+      contractAddress,
       contractName: "test-trait",
       functionName: "call-foo",
       functionArgs: [
-        contractPrincipalCV(Accounts.DEPLOYER.stxAddress, "impl-trait"),
+        contractPrincipalCV(contractAddress, "impl-trait"),
         uintCV(3),
       ],
-      fee: 2000,
+      fee,
       network,
       anchorMode: AnchorMode.OnChainOnly,
       postConditionMode: PostConditionMode.Allow,
       nonce: 0,
     };
-    transaction = await makeContractCall(callTxOptions);
-    response = await broadcastTransaction(transaction, network);
+    let transaction = await makeContractCall(callTxOptions);
+    let response = await broadcastTransaction(transaction, network);
     let [_, tx] = await asyncExpectStacksTransactionSuccess(
       orchestrator,
       transaction.txid()
     );
     expect((tx as StacksTransactionMetadata).result).toEqual("(ok u3)");
+  });
+
+  it("passing a trait parameter should work not work in Stacks 2.2", async () => {
+    const network = new StacksTestnet({
+      url: orchestrator.getStacksNodeUrl(),
+    });
+    const contractAddress = Accounts.DEPLOYER.stxAddress;
 
     // Wait for the 2.2 activation, then check again
     await orchestrator.waitForStacksBlockAnchoredOnBitcoinBlockOfHeight(
-      timeline.epoch_2_2
+      timeline.epoch_2_2 + 2
     );
+
+    // deploy wrapper
+    let deployResponse = await deployContract(
+      network,
+      Accounts.DEPLOYER.secretKey,
+      2,
+      "test-trait-wrapper",
+      `(define-trait test-trait
+        ((foo (uint) (response uint uint))))
+      
+      (define-public (call-foo (f <test-trait>) (a uint))
+        (contract-call? .test-trait call-foo f a)
+      )`
+    );
+    await asyncExpectStacksTransactionSuccess(
+      orchestrator,
+      deployResponse.transaction.txid()
+    );
+
+    // Call public function as readonly
+    let output = await callReadOnlyFunction({
+      contractName: "test-trait",
+      contractAddress,
+      functionName: "call-foo",
+      functionArgs: [
+        contractPrincipalCV(contractAddress, "impl-trait"),
+        uintCV(1),
+      ],
+      network,
+      senderAddress: Accounts.WALLET_1.stxAddress,
+    });
+    expect(output).toEqual(responseOkCV(uintCV(1)));
 
     // Call the readonly function
     output = await callReadOnlyFunction({
@@ -168,27 +210,54 @@ describe("trait parameter", () => {
     );
 
     // Call the public function
-    callTxOptions = {
+    let callTxOptions = {
       senderKey: Accounts.WALLET_1.secretKey,
-      contractAddress: Accounts.DEPLOYER.stxAddress,
+      contractAddress,
       contractName: "test-trait",
       functionName: "call-foo",
       functionArgs: [
-        contractPrincipalCV(Accounts.DEPLOYER.stxAddress, "impl-trait"),
+        contractPrincipalCV(contractAddress, "impl-trait"),
         uintCV(3),
       ],
-      fee: 2000,
+      fee,
       network,
       anchorMode: AnchorMode.OnChainOnly,
       postConditionMode: PostConditionMode.Allow,
       nonce: 1,
     };
-    transaction = await makeContractCall(callTxOptions);
-    response = await broadcastTransaction(transaction, network);
-    [_, tx] = await asyncExpectStacksTransactionSuccess(
+    let transaction = await makeContractCall(callTxOptions);
+    await broadcastTransaction(transaction, network);
+
+    let [_, tx] = await waitForStacksTransaction(
       orchestrator,
       transaction.txid()
     );
-    expect((tx as StacksTransactionMetadata).result).toEqual("(ok u3)");
+    expect(tx.result).toEqual("(ok u3)");
   });
 });
+
+async function deployContract(
+  network: StacksTestnet,
+  senderKey: string,
+  nonce: number,
+  contractName: string,
+  codeBody: string
+) {
+  // Build the transaction to deploy the contract
+  let deployTxOptions = {
+    senderKey,
+    contractName,
+    codeBody,
+    fee,
+    network,
+    anchorMode: AnchorMode.OnChainOnly,
+    postConditionMode: PostConditionMode.Allow,
+    nonce,
+    clarityVersion: undefined,
+  };
+
+  let transaction = await makeContractDeploy(deployTxOptions);
+  let response = await broadcastTransaction(transaction, network);
+  expect(response.error).toBeUndefined();
+  return { transaction, response };
+}
