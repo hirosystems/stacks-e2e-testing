fc793648848922665a4b2febda9be78b3cfc3661
Brice Dobry <brice@hiro.so>
2023-05-05 11:09:13 -0400

    feat: improve epoch timelines
    Allow tests to specify any epoch settings, then fill in the rest with
    reasonable defaults.

--- tests/integration/helpers.ts
+++ tests/integration/helpers.ts
@@ -19,13 +19,13 @@ import {
 const fetch = require("node-fetch");
 
 interface EpochTimeline {
-  epoch_2_0: number;
-  epoch_2_05: number;
-  epoch_2_1: number;
-  pox_2_activation: number;
-  epoch_2_2: number;
-  epoch_2_3: number;
-  epoch_2_4: number;
+  epoch_2_0?: number;
+  epoch_2_05?: number;
+  epoch_2_1?: number;
+  pox_2_activation?: number;
+  epoch_2_2?: number;
+  epoch_2_3?: number;
+  epoch_2_4?: number;
 }
 
 export const DEFAULT_EPOCH_TIMELINE = {
@@ -38,6 +38,50 @@ export const DEFAULT_EPOCH_TIMELINE = {
   epoch_2_4: Constants.DEVNET_DEFAULT_EPOCH_2_4,
 };
 
+export const POX_CYCLE_LENGTH = 10;
+
+/// This function will fill in any missing epoch values in the timeline
+/// with reasonable block heights.
+function fillTimeline(timeline: EpochTimeline) {
+  if (timeline.epoch_2_0 === undefined) {
+    timeline.epoch_2_0 = DEFAULT_EPOCH_TIMELINE.epoch_2_0;
+  }
+  if (timeline.epoch_2_05 === undefined) {
+    timeline.epoch_2_05 = DEFAULT_EPOCH_TIMELINE.epoch_2_05;
+    while (timeline.epoch_2_05 <= timeline.epoch_2_0) {
+      timeline.epoch_2_05 += POX_CYCLE_LENGTH;
+    }
+  }
+  if (timeline.epoch_2_1 === undefined) {
+    timeline.epoch_2_1 = DEFAULT_EPOCH_TIMELINE.epoch_2_1;
+    while (timeline.epoch_2_1 <= timeline.epoch_2_05) {
+      timeline.epoch_2_1 += POX_CYCLE_LENGTH;
+    }
+  }
+  if (timeline.pox_2_activation === undefined) {
+    timeline.pox_2_activation = timeline.epoch_2_1 + 1;
+  }
+  if (timeline.epoch_2_2 === undefined) {
+    timeline.epoch_2_2 = DEFAULT_EPOCH_TIMELINE.epoch_2_2;
+    while (timeline.epoch_2_2 <= timeline.pox_2_activation) {
+      timeline.epoch_2_2 += POX_CYCLE_LENGTH;
+    }
+  }
+  if (timeline.epoch_2_3 === undefined) {
+    timeline.epoch_2_3 = DEFAULT_EPOCH_TIMELINE.epoch_2_3;
+    while (timeline.epoch_2_3 <= timeline.epoch_2_2) {
+      timeline.epoch_2_3 += POX_CYCLE_LENGTH;
+    }
+  }
+  if (timeline.epoch_2_4 === undefined) {
+    timeline.epoch_2_4 = DEFAULT_EPOCH_TIMELINE.epoch_2_4;
+    while (timeline.epoch_2_4 <= timeline.epoch_2_3) {
+      timeline.epoch_2_4 += POX_CYCLE_LENGTH;
+    }
+  }
+  return timeline;
+}
+
 export function buildDevnetNetworkOrchestrator(
   networkId: number,
   timeline: EpochTimeline = DEFAULT_EPOCH_TIMELINE,
@@ -46,6 +90,8 @@ export function buildDevnetNetworkOrchestrator(
 ) {
   let uuid = Date.now();
   let working_dir = `/tmp/stacks-test-${uuid}-${networkId}`;
+  // Fill in default values for any unspecified epochs
+  let full_timeline = fillTimeline(timeline);
   // Set the stacks-node image URL to the default image for the version if it's
   // not explicitly set
   if (stacks_node_image_url === undefined) {
@@ -56,13 +102,13 @@ export function buildDevnetNetworkOrchestrator(
     devnet: {
       name: `ephemeral-devnet-${uuid}`,
       bitcoin_controller_block_time: Constants.BITCOIN_BLOCK_TIME,
-      epoch_2_0: timeline.epoch_2_0,
-      epoch_2_05: timeline.epoch_2_05,
-      epoch_2_1: timeline.epoch_2_1,
-      pox_2_activation: timeline.pox_2_activation,
-      epoch_2_2: timeline.epoch_2_2,
-      epoch_2_3: timeline.epoch_2_3,
-      epoch_2_4: timeline.epoch_2_4,
+      epoch_2_0: full_timeline.epoch_2_0,
+      epoch_2_05: full_timeline.epoch_2_05,
+      epoch_2_1: full_timeline.epoch_2_1,
+      pox_2_activation: full_timeline.pox_2_activation,
+      epoch_2_2: full_timeline.epoch_2_2,
+      epoch_2_3: full_timeline.epoch_2_3,
+      epoch_2_4: full_timeline.epoch_2_4,
       bitcoin_controller_automining_disabled: false,
       working_dir,
       use_docker_gateway_routing: process.env.GITHUB_ACTIONS ? true : false,
