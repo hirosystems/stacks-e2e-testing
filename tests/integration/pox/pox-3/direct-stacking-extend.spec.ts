900695f8cd0489ab56db85ebfbc4f9e26ec1e07b
Brice Dobry <brice@hiro.so>
2023-05-07 12:40:32 -0400

    test: direct stacking extend

---
+++ tests/integration/pox/pox-3/direct-stacking-extend.spec.ts
@@ -0,0 +1,117 @@
+import { DevnetNetworkOrchestrator } from "@hirosystems/stacks-devnet-js";
+import { StacksTestnet } from "@stacks/network";
+import { ClarityValue, uintCV } from "@stacks/transactions";
+import { Accounts } from "../../constants";
+import {
+  buildDevnetNetworkOrchestrator,
+  getChainInfo,
+  getNetworkIdFromEnv,
+  waitForStacksTransaction,
+} from "../../helpers";
+import {
+  getPoxInfo,
+  readRewardCyclePoxAddressForAddress,
+  waitForNextRewardPhase,
+} from "../helpers";
+import {
+  broadcastStackExtend,
+  broadcastStackSTX,
+} from "../helpers-direct-stacking";
+
+describe("testing stack-extend functionality", () => {
+  let orchestrator: DevnetNetworkOrchestrator;
+  let timeline = {
+    epoch_2_0: 100,
+    epoch_2_05: 102,
+    epoch_2_1: 104,
+    pox_2_activation: 105,
+    epoch_2_2: 106,
+    epoch_2_3: 108,
+    epoch_2_4: 112,
+  };
+  const fee = 1000;
+  let aliceNonce = 0;
+
+  beforeAll(() => {
+    orchestrator = buildDevnetNetworkOrchestrator(
+      getNetworkIdFromEnv(),
+      timeline
+    );
+    orchestrator.start();
+  });
+
+  afterAll(() => {
+    orchestrator.terminate();
+  });
+
+  it("stacking and then extending should result in rewards for 3 cycles", async () => {
+    const network = new StacksTestnet({ url: orchestrator.getStacksNodeUrl() });
+
+    // Wait for 2.4 to go live
+    await orchestrator.waitForStacksBlockAnchoredOnBitcoinBlockOfHeight(
+      timeline.epoch_2_4
+    );
+    await orchestrator.waitForNextStacksBlock();
+
+    let chainInfo = await getChainInfo(network);
+    const blockHeight = chainInfo.burn_block_height;
+
+    // Alice stacks 80m STX for 1 cycle
+    let response = await broadcastStackSTX(
+      {
+        poxVersion: 3,
+        network,
+        account: Accounts.WALLET_1,
+        fee,
+        nonce: aliceNonce++,
+      },
+      {
+        amount: 80_000_000_000_000,
+        blockHeight,
+        cycles: 1,
+      }
+    );
+    expect(response.error).toBeUndefined();
+    // Wait for Alice's stacking transaction to confirm
+    let [block, tx] = await waitForStacksTransaction(
+      orchestrator,
+      response.txid
+    );
+    expect(tx.success).toBeTruthy();
+
+    // Alice extends stacking for another 2 cycles
+    response = await broadcastStackExtend(
+      {
+        poxVersion: 3,
+        network,
+        account: Accounts.WALLET_1,
+        fee,
+        nonce: aliceNonce++,
+      },
+      { cycles: 2 }
+    );
+    expect(response.error).toBeUndefined();
+    // Wait for Alice's stacking extension transaction to confirm
+    [block, tx] = await waitForStacksTransaction(orchestrator, response.txid);
+    expect(tx.success).toBeTruthy();
+
+    // Check rewards for 3 cycles
+    for (let cycle = 1; cycle <= 3; cycle++) {
+      let poxInfo = await getPoxInfo(network);
+      // Asserts about pox info for better knowledge sharing
+      expect(poxInfo.contract_id).toBe("ST000000000000000000002AMW42H.pox-3");
+      expect(poxInfo.current_cycle.id).toBe(cycle);
+
+      const poxAddrInfo = (await readRewardCyclePoxAddressForAddress(
+        network,
+        3,
+        cycle + 1, // cycle + 1 because we are checking the next cycle, including rewards
+        Accounts.WALLET_1.stxAddress
+      )) as Record<string, ClarityValue>;
+      expect(poxAddrInfo?.["total-ustx"]).toEqual(uintCV(80_000_000_000_000));
+
+      // Wait for 1 reward cycle
+      await waitForNextRewardPhase(network, orchestrator, 1);
+    }
+  });
+});
